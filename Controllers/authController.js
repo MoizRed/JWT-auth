bbb1e7d62c1a50e82521571589eaeb6348c87d2f
MoizRed <abdoureduniv2000@gmail.com>
2024-10-23 16:15:45 +0100

    some changed

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,10 +1,3 @@
-const hash = require("hash")
-
-const mongoose = require("mongoose");
-
-const User = require("../models/User");
-
-
 module.exports.signup_get = (req, res) => {
   res.render("../views/signup.ejs");
 };
@@ -13,23 +6,11 @@ module.exports.login_get = (req, res) => {
   res.render("../views/login.ejs");
 };
 
-module.exports.signup_post = async (req, res) => {
-  const { email, password } = req.body;
+module.exports.signup_post = (req, res) => {
+  const body = req.body;
+  console.log(body);
 
-
-  try {
-    const user = await User.create(
-      {
-        email,
-        password,
-      },
-      console.log("created.")
-    );
-    res.send("created");
-  } catch (err) {
-    console.log(err);
-    res.status(400).send("error botch");
-  }
+  res.send("created");
 };
 
 module.exports.login_post = (req, res) => {
